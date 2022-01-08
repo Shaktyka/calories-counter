0d0207cd798a6d101010c36eda6198df898f4f95
Shaktyka <k-i-s-u-n-j-a@list.ru>
2022-01-08 22:54:59 +0500

    Реализует управление состоянием кнопок расчёта и очистки

--- js/script.js
+++ js/script.js
@@ -5,27 +5,47 @@ import {activityCoefficient, maintainNormative, weightGainPercentage, weightLoss
 const form = document.querySelector('.form');
 const submitBtn = form.querySelector('.form__submit-button');
 const resetButton = form.querySelector('.form__reset-button');
-const inputsGroups = form.querySelector('.inputs-group');
+const inputsGroup = form.querySelector('.inputs-group');
+const ageInput = form.querySelector('#age');
+const heightInput = form.querySelector('#height');
+const weightInput = form.querySelector('#weight');
 // HTML-элементы блока вывода:
 const resultBlock = document.querySelector('.counter__result');
 const caloriesNormValue = resultBlock.querySelector('#calories-norm');
 const caloriesMinValue = resultBlock.querySelector('#calories-minimal');
 const caloriesMaxValue = resultBlock.querySelector('#calories-maximal');
 
+// Переменные:
+let ageValue = null;
+let heightValue = null;
+let weightValue = null;
+
 // Сбрасывает значения формы в начальные:
 const resetForm = () => {
   form.reset;
+  resultBlock.classList.add('counter__result--hidden');
 };
 
-// Разблокирует кнопку сброса, если введено какое-то значение в поля:
-inputsGroups.addEventListener('input', (evt) => {
-  const target = evt.target;
-  if (target.value !== '') {
+// Обработчик ввода в числовые поля:
+const inputsGroupInputHandler = (evt) => {
+  ageValue = ageInput.value;
+  heightValue = heightInput.value;
+  weightValue = weightInput.value;
+
+  // Переключает состояние кнопки "Очистить поля":
+  if (ageValue !== '' || heightValue !== '' || weightValue !== '') {
     resetButton.disabled = false;
   } else {
     resetButton.disabled = true;
   }
-});
+
+  // Переключает состояние кнопки "Рассчитать":
+  if (ageValue !== '' && heightValue !== '' && weightValue !== '') {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+};
 
 // Обработчик отправки формы:
 const formSubmitHandler = (evt) => {
@@ -40,6 +60,8 @@ const init = () => {
 
   // Вешает обработчик на форму:
   form.addEventListener('submit', formSubmitHandler);
+  // Вешает обработчик на группу инпутов:
+  inputsGroup.addEventListener('input', inputsGroupInputHandler);
 };
 
 // Вызов стартовой ф-ции:
